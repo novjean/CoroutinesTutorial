714ada719c2bcff6943778bb8415e3e452cb7d2d
Novjean J Kannathara <novjeanj@gmail.com>
2024-10-18 13:49:22 +0530

    global scope

--- app/src/main/java/com/novatech/coroutinestutorial/MainActivity.kt
+++ app/src/main/java/com/novatech/coroutinestutorial/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.novatech.coroutinestutorial
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,29 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+//        GlobalScope.launch {
+//            delay(3000L)
+//            Log.d(TAG, "Coroutine says hello from thread ${Thread.currentThread().name}")
+//        }
+//        Log.d(TAG, "Hello from thread ${Thread.currentThread().name}")
+
+        GlobalScope.launch {
+            val networkCallAnswer = doNetworkCall()
+            Log.d(TAG, networkCallAnswer)
+
+            val networkCallAnswer2 = doNetworkCall2()
+            Log.d(TAG, networkCallAnswer2)
+        }
+    }
+
+    suspend fun doNetworkCall(): String{
+        delay(3000L)
+        return "This is the answer"
+    }
+
+    suspend fun doNetworkCall2(): String{
+        delay(3000L)
+        return "This is the 2nd answer"
     }
 }
